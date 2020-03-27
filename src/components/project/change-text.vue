97e457e38d665a37d89646d38dd4b2edfe2866a1
vlad <elsolo.dev@gmail.com>
2020-03-27 14:37:11 +0200

    added change-text.vue component

---
+++ src/components/project/change-text.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="textValue" type="text">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ChangeText extends Vue {
+  @Prop()
+  private text!: string
+
+  get textValue () {
+    return this.text
+  }
+
+  set textValue (value) {
+    this.$emit('updateText', value)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
